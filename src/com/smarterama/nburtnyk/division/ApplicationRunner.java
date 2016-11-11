a2837ffd3449acd3c633d4e99fb58f80d255ff4c
Natalia Burtnyk <nataliaburtnyk@gmail.com>
2016-11-11 16:17:32 +0200

    It is work

--- src/com/smarterama/nburtnyk/division/ApplicationRunner.java
+++ src/com/smarterama/nburtnyk/division/ApplicationRunner.java
@@ -2,4 +2,9 @@ package com.smarterama.nburtnyk.division;
 
 public class ApplicationRunner {
 
+	public static void main(String[] args) {
+		Division division = new Division();
+		division.start("40/3");
+		
+	}
 }
