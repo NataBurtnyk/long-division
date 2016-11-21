4e229bf2501323ee80554f23c3416ee88fdedab0
Natalia Burtnyk <nataliaburtnyk@gmail.com>
2016-11-21 23:47:56 +0200

    Was added test with zero divisor

--- src/com/smarterama/nburtnyk/division/LongDivision.java
+++ src/com/smarterama/nburtnyk/division/LongDivision.java
@@ -1,8 +1,5 @@
 package com.smarterama.nburtnyk.division;
 
-import java.util.List;
-import java.util.ArrayList;
-
 public class LongDivision {
 	
 	private int dividend;
@@ -10,18 +7,17 @@ public class LongDivision {
 	private String result;
 	
 	public LongDivision(int dividend, int divisor) {
+		checkDivisor(divisor);
 		this.dividend = dividend;
 		this.divisor = divisor;
-		this.result = "";
-		printHeader();
 	}
-
-//	private String repeatString(String s, int n) {
-//		return new String(new char[n]).replace("\0", s);
-//	}
-//	
-	public void printHeader() {
-
+	
+	private void checkDivisor(int divisor){
+		if (divisor == 0) {
+			throw new IllegalArgumentException("Divisor can not be 0"); 
+		}
 	}
+	
+	
 	
 }
--- test/com/smarterama/nburtnyk/division/IntegerDivisionTest.java
+++ test/com/smarterama/nburtnyk/division/IntegerDivisionTest.java
@@ -1,5 +1,18 @@
 package com.smarterama.nburtnyk.division;
 
-public class IntegerDivisionTest {
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
+public class IntegerDivisionTest {
+		
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
+	
+	@Test
+	public void testConditionWithNull() {
+		exception.expect(IllegalArgumentException.class);
+		exception.expectMessage("Divisor can not be 0");
+		new LongDivision(744, 0);
+	}
 }
