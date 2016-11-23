3468fc6663acb3a43339f1d7eec0184388901361
Natalia Burtnyk <nataliaburtnyk@gmail.com>
2016-11-23 13:34:29 +0200

    Some refactore

--- src/com/smarterama/nburtnyk/division/ApplicationRunner.java
+++ src/com/smarterama/nburtnyk/division/ApplicationRunner.java
@@ -3,7 +3,7 @@ package com.smarterama.nburtnyk.division;
 public class ApplicationRunner {
 
 	public static void main(String[] args) {
-		LongDivision division = new LongDivision(-9, 3);
+		LongDivision division = new LongDivision(8, 20);
 		division.divideWithOutput();
 	}
 }
